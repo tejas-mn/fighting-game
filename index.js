58020ae51ee37195cae2285e21fa6a328355abaa
tejas-mn <tejasmn2020@gmail.com>
2022-04-23 18:24:28 +0530

    Movements Done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvas  = document.querySelector('canvas');
 const cx = canvas.getContext('2d');
-const gravity = 0.3
+const gravity = 0.7
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -15,6 +15,7 @@ class Sprite{
         this.velocity = velocity;
         this.height = height;
         this.width = width;
+        this.lastKey
     }
 
     draw(){
@@ -27,6 +28,7 @@ class Sprite{
         this.draw()
         this.position.y+=this.velocity.y;
         this.position.x+=this.velocity.x;
+
         //to stop sprite cross canvas height
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y=0 //stops the sprite
@@ -64,6 +66,21 @@ const enemy = new Sprite({
 player.draw();
 enemy.draw();
 
+const keys = {
+    a:{
+        pressed:false
+    },
+    d:{
+        pressed:false
+    },
+    ArrowLeft:{
+        pressed:false
+    },
+    ArrowRight:{
+        pressed:false
+    }
+}
+
 function animateCallback(){
     requestAnimationFrame(animateCallback);
     //console.log('animate');
@@ -71,6 +88,23 @@ function animateCallback(){
     cx.fillRect(0,0,canvas.width , canvas.height);
     player.update();
     enemy.update();
+    
+    player.velocity.x=0
+    enemy.velocity.x=0
+
+    //player  movement
+    if(keys.a.pressed && player.lastKey=='a'){
+        player.velocity.x=-5
+    }else if(keys.d.pressed && player.lastKey=='d'){
+        player.velocity.x=5
+    }
+
+    //enemy movement
+    if(keys.ArrowLeft.pressed && enemy.lastKey=='ArrowLeft'){
+        enemy.velocity.x=-5
+    }else if(keys.ArrowRight.pressed && enemy.lastKey=='ArrowRight'){
+        enemy.velocity.x=5
+    }
 }
 
 animateCallback();
@@ -78,10 +112,19 @@ animateCallback();
 addEventListener('keyup' , (event)=>{
     console.log(event.key);
     switch(event.key){
-        case 'd' :  player.velocity.x = 0
-                    break
-        case 'a' : player.velocity.x = 0
-                   break
+        case 'd':   keys.d.pressed=false
+                    break   
+        case 'a':   keys.a.pressed=false
+                    break     
+    }
+
+    switch(event.key){
+        case 'ArrowRight':   
+        keys.ArrowRight.pressed=false
+        break   
+        case 'ArrowLeft':   
+        keys.ArrowLeft.pressed=false
+        break     
     }
     // console.log(event.key);
 });
@@ -89,11 +132,34 @@ addEventListener('keyup' , (event)=>{
 addEventListener('keydown' , (event)=>{
     console.log(event.key);
     switch(event.key){
-        
-        case 'd' :  player.velocity.x = 10
-                    break
-        case 'a' : player.velocity.x = -10
-                   break
+        case 'd':   
+        keys.d.pressed=true
+        player.lastKey = 'd'
+        break    
+
+        case 'a':   
+        keys.a.pressed=true
+        player.lastKey = 'a'
+        break
+
+        case 'w':   
+        player.velocity.y = -20
+        break
+
+        case 'ArrowRight':   
+        keys.ArrowRight.pressed=true
+        enemy.lastKey = 'ArrowRight'
+        break    
+
+        case 'ArrowLeft':   
+        keys.ArrowLeft.pressed=true
+        enemy.lastKey = 'ArrowLeft'
+        break
+
+        case 'ArrowUp': 
+        enemy.velocity.y = -20
+        break
+
     }
     // console.log(event.key);
 });
